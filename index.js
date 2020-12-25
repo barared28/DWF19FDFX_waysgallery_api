e4733edf1553ded225324a8a836d4f6e7b538855
barared28 <barared28@gmail.com>
2020-12-25 09:09:11 +0700

    setting deploy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 5000;
+const port = process.env.PORT || 5000;
 const router = require("./src/routes");
 
 require("dotenv").config();
@@ -14,6 +14,4 @@ app.use(express.json());
 
 app.use("/api/v1", router);
 
-app.listen(port, () => {
-  console.log(`server running on ${port}`);
-});
+app.listen(port);
